1f49994d5a68008bb423662d2d23731dea570f66
Priyanshu <prysh310704@gmail.com>
2026-05-04 22:19:06 +0530

    added health

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 app.get('/',(req,res)=>{
     res.send("App is Running Letsss Go!!! ✈️");
 });
+app.get('/health',(req,res)=>{
+    res.send("App is healthy");
+});
 app.listen(3000,()=>{
     console.log("App is Running on port 3000");
 });
