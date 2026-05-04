1e1ac5ac158c57d8d175ec7ca3dfc3836e406e9e
Priyanshu <150844486+Priyanshu-310704@users.noreply.github.com>
2026-05-04 22:31:59 +0530

    Update health check message with emoji

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ app.get('/',(req,res)=>{
     res.send("App is Running Letsss Go!!! ✈️");
 });
 app.get('/health',(req,res)=>{
-    res.send("App is healthy+++");
+    res.send("App is healthy🍎");
 });
 app.listen(3000,()=>{
     console.log("App is Running on port 3000");
